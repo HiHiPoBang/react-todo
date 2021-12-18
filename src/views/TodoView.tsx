da0e22e1ea29110677713de0fc92fbcedae3d2b1
Iris Pai <irispai@inquartik.com>
2021-12-18 21:54:14 +0800

    init: remove not used code and add views

---
+++ src/views/TodoView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoView.scss';
+
+const TodoView = function () {
+  return (
+    <div className="TodoView">
+      This is todo view
+    </div>
+  );
+};
+
+export default TodoView;
